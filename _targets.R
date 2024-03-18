26464baebb293b46e6e1b2aab2dfc79e874e85aa
frederikkenatorp <frederikke.natorp@gmail.com>
2024-03-18 16:21:37 +0100

    Creating the GBIF+field data output

--- _targets.R
+++ _targets.R
@@ -105,6 +105,7 @@ tar_target(Long_Buffer_gbif, make_long_buffer(DT = buffer_500_gbif),
 
 #Generating a phylogenetic tree of the observed species
   tar_target(Phylo_Tree, generate_tree(Presences)),
+  tar_target(Phylo_Tree_field, generate_tree(joint_data)),
 
 #Modelling the the species distribution based on the habitat types in the raster map of present nature
   #tar_target(ModelAndPredict, ModelAndPredictFunc(DF =  Presences, file = LandUseTiff),
@@ -121,10 +122,20 @@ tar_target(Long_Buffer_gbif, make_long_buffer(DT = buffer_500_gbif),
              Make_Look_Up_Table("species_lookup.xlsx")),
   tar_target(LanduseTable, generate_landuse_table(path = LanduseSuitability)),
   tar_target(Long_LU_table, Make_Long_LU_table(DF = LanduseTable)),
+
+#Final presences GBIF:
   tar_target(Final_Presences, make_final_presences(Long_LU_table, Long_Buffer_gbif, LookUpTable),
              pattern = map(Long_Buffer_gbif),
              iteration = "group"),
   tarchetypes::tar_group_by(joint_final_presences, as.data.frame(Final_Presences), Landuse),
+
+#Final presences GBIF+Field:
+  tar_target(Final_Presences_field, make_final_presences(Long_LU_table, Long_Buffer_field, LookUpTable),
+             pattern = map(Long_Buffer_field),
+             iteration = "group"),
+  tarchetypes::tar_group_by(joint_final_presences_field, as.data.frame(Final_Presences_field), Landuse),
+
+#output for GBIF data:
   tar_target(rarity_weight, calc_rarity_weight(joint_final_presences)),
   tar_target(rarity, calc_rarity(joint_final_presences, rarity_weight),
              map(joint_final_presences),
@@ -145,6 +156,29 @@ tar_target(Long_Buffer_gbif, make_long_buffer(DT = buffer_500_gbif),
   tar_target(name = output_Rarity,
              command = export_rarity(Results = rarity, path = LandUseTiff),
              map(rarity),
+             format = "file"),
+
+#output for the GBIF+field data:
+  tar_target(rarity_weight_field, calc_rarity_weight(joint_final_presences_field)),
+  tar_target(rarity_field, calc_rarity(joint_final_presences_field, rarity_weight_field),
+             map(joint_final_presences_field),
+             iteration = "group"),
+  tar_target(PhyloDiversity_field,
+             calc_pd(joint_final_presences_field, Phylo_Tree_field),
+             map(joint_final_presences_field),
+             iteration = "group"),
+  tar_target(Richness_field, GetRichness(joint_final_presences_field)),
+  tar_target(name = output_Richness_field,
+             command = export_richness_field(Results = PhyloDiversity_field, path = LandUseTiff),
+             map(PhyloDiversity_field),
+             format = "file"),
+  tar_target(name = output_PD_field,
+             command = export_pd_field(Results = PhyloDiversity_field, path = LandUseTiff),
+             map(PhyloDiversity_field),
+             format = "file"),
+  tar_target(name = output_Rarity_field,
+             command = export_rarity_field(Results = rarity_field, path = LandUseTiff),
+             map(rarity_field),
              format = "file")
 )
 
